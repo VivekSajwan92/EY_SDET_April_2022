89df4b8e5469ef5afc8df26558c040353920edcb
TestLeafSDET <103097499+TestLeafSDET@users.noreply.github.com>
2022-04-29 11:27:12 +0530

    Day18

--- datastrucutresAndAlgorithms/ey/training/week4/day18/MaximumBalloon.java
+++ datastrucutresAndAlgorithms/ey/training/week4/day18/MaximumBalloon.java
@@ -1,5 +1,30 @@
 package datastrucutresAndAlgorithms.ey.training.week4.day18;
 
-public class MaximumBalloon {
+import java.util.HashMap;
+import java.util.Map;
 
+public class MaximumBalloon {
+	public int maxNumberOfBalloons(String text) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        HashMap<Character, Integer> requirementMap = new HashMap<>();
+        requirementMap.put('b',1);
+        requirementMap.put('a',1);
+        requirementMap.put('l',2);
+        requirementMap.put('o',2);
+        requirementMap.put('n',1);
+
+        for(char each : text.toCharArray())
+            map.put(each, map.getOrDefault(each, 0) + 1);
+
+        if(requirementMap.size() > map.size()) return 0;
+
+        int output = Integer.MAX_VALUE;
+
+        for(Map.Entry<Character, Integer> entry : requirementMap.entrySet()) {
+            if(requirementMap.containsKey(entry.getKey()))
+                output = Math.min(output, map.get(entry.getKey())/(entry.getValue()));
+        }
+
+        return output;
+    }
 }
